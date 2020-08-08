87a3dbd0f6193b3ac04be93533bcfb122e62f858
muhammadzee343 <muhammad.zee343@gmail.com>
2020-08-08 14:59:07 +0500

    add screens

---
+++ screens/CategoriesMealScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const CategoryMealScreen = () => {
+  return (
+    <View>
+      <Text>The CategoriesMeal screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CategoryMealScreen;
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const MealDetailScreen = () => {
+  return (
+    <View>
+      <Text>The MealDetail screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default MealDetailScreen;
