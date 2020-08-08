bd236744e2245c94baf9505b98e8190bab532de0
muhammadzee343 <muhammad.zee343@gmail.com>
2020-08-08 16:28:50 +0500

    add fonts and splash

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
-function App() {
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View>
-      <Text>Meal App With Navigation</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
-
-export default App;
