06117e6245b787df80ccb977ce8f57dbb768b36d
muhammadzee343 <muhammad.zee343@gmail.com>
2020-08-08 14:33:52 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Meal App With Navigation</Text>
+    </View>
+  );
+}
+
+export default App;
